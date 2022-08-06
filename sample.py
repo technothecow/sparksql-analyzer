ea5533c5675a9febbc9588058d9f62510805c79a
michael <krashmisha@gmail.com>
2022-08-06 13:59:04 +0300

    Huge revamp of SELECT grammar rules and update of the sample file

--- sample.py
+++ sample.py
@@ -1,4 +1,13 @@
 from analyzer import Analyzer
 
-a=Analyzer('grammar.lark')
-print(a.analyze('SELECT a, b FROM c WHERE a>b;'))
+a=Analyzer('test_grammar.lark')
+sample='''SELECT City FROM Customers
+UNION
+SELECT City1 FROM Suppliers
+UNION ALL
+SELECT City2 FROM Suppliers
+UNION ALL
+SELECT City3 FROM Suppliers
+GROUP BY City4;'''
+res=a.analyze(sample)
+print(res.pretty())
